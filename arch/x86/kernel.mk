ce6e6be8b15929c1464bccd8b90d467b2f956b64
Tim Detwiler <timdetwiler@gmail.com>
2012-09-15 09:02:42 -0400

    Add boot targets to arch makefiles.
    Add the target 'boot' and 'gdb-boot' to the x86 and arm makefiles. These
    target will build a kernel, and boot using qemu.
    
    On x86 this requires a simple grub wrapper/floppy disk image, while arm
    can just pass the elf to qemu via '-kernel'.
    
    gdb-boot will tell qemu to wait for a debugger to attach before
    executing any code.
    
    Change-Id: I2454a54696f2e8532911c13a40af5c960390514d
    Signed-off-by: Tim Detwiler <timdetwiler@gmail.com>

--- arch/x86/kernel.mk
+++ arch/x86/kernel.mk
@@ -1,6 +1,26 @@
+ARCH_DIR = arch/x86
+BOOTFDDIR = $(ARCH_DIR)/bootfd
 
 CFLAGS += -fno-omit-frame-pointer -fno-stack-protector -gdwarf-2 -m32
 LDFLAGS += -melf_i386 -Ttext 100000
+KCLEANS += bootfd.img
 
 KERNEL_BOOT_HEAD = head.o
 ARCH_OBJS += entry.o
+
+QEMU ?= qemu-system-i386
+QEMU_BOOT = $(QEMU) -fda bootfd.img
+
+boot: bootfd.img
+	$(QEMU_BOOT)
+
+gdb-boot: bootfd.img
+	$(QEMU_BOOT) -s -S
+
+bootfd.img: vmtimiz
+	@echo '  (GUNZIP): ' bootfd.img
+	@gzip -cd $(BOOTFDDIR)/bootfd.img.gz > bootfd.img
+	@echo '   (MCOPY): ' bootfd.img
+	@mcopy -o -i "bootfd.img" vmtimiz \
+													 $(BOOTFDDIR)/menu.lst \
+													 ::/boot/
