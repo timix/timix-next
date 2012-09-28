9177cc7285c040cfde2a932a7d9325598e27d5a3
Tim Detwiler <timdetwiler@gmail.com>
2012-09-28 13:23:17 -0400

    nios2: Add srec target.
    In order to utilize the Altera board flashing tools, we need to get the
    kernel into the 'srec' format. This target depends on tools in the
    altera toolchain.
    
    Some parameters in this target (base, end, entry) will be board
    dependant and are currently filled with place-holder values (meaning the
    target is non-functional at this time).
    
    Change-Id: I0b360786622970808a623f452c1eba8e38316075
    Signed-off-by: Tim Detwiler <timdetwiler@gmail.com>

--- arch/nios2/kernel.mk
+++ arch/nios2/kernel.mk
@@ -1,7 +1,32 @@
 ARCH_DIR=arch/nios2
+NIOS_DIR?=/opt/altera/11.1sp1/nios2eds/
+
+E2F := java -jar $(NIOS_DIR)/bin/elf2flash.jar
 
 INCLUDES += -I$(ARCH_DIR)/include/
-
 KERNEL_BOOT_HEAD = kernel/head.o
-
 ARCH_OBJS += kernel/entry.o kernel/atomic.o kernel/io.o
+
+###############################################################################
+# Targets
+###############################################################################
+#
+# SREC target
+#
+.PHONY: srec
+srec: vmtimix.srec
+
+#
+# Builds an srec file suitable for being programmed.
+#
+# --input:  Input elf file of the kernel
+# --output: SREC output.
+# --base:   Flash memory base address.
+# --end:    Flash memory end address.
+# --reset:  Initial address.
+#
+SREC=vmtimix.srec
+KVERYCLEANS += $(SREC)
+$(SREC): $O/vmtimix
+	@echo '    (SREC): ' $@
+	$Q$(E2F) --input=$< --output=$@ --base=0 --end=0x8000 --reset=0
